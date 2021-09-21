e512f6ac71831e6f8c41fb7130bf36b479d7fe68
Steven Zeiler <me@stevezeiler.com>
2021-09-21 20:27:57 +0200

    Job search sorting by more fields

--- lib/proofofwork/boost_job.ex
+++ lib/proofofwork/boost_job.ex
@@ -1,7 +1,7 @@
 defmodule Proofofwork.BoostJob do
   use Ecto.Schema
   import Ecto.Changeset
-  @derive {Jason.Encoder, only: [:content, :difficulty, :category, :tag, :txid, :value, :vout, :timestamp, :additionalData]}
+  @derive {Jason.Encoder, only: [:content, :difficulty, :category, :tag, :txid, :value, :vout, :additionalData]}
 
   schema "boost_jobs" do
     field :additionalData, :string
@@ -9,7 +9,7 @@ defmodule Proofofwork.BoostJob do
     field :content, :string
     field :difficulty, :decimal
     field :tag, :string
-    field :timestamp, :naive_datetime
+    #field :timestamp, :naive_datetime
     field :txid, :string
     field :userNonce, :string
     field :value, :integer
@@ -21,7 +21,7 @@ defmodule Proofofwork.BoostJob do
   @doc false
   def changeset(boost_job, attrs) do
     boost_job
-    |> cast(attrs, [:content, :difficulty, :category, :tag, :additionalData, :userNonce, :txid, :vout, :value, :timestamp])
-    |> validate_required([:content, :difficulty, :category, :tag, :additionalData, :userNonce, :txid, :vout, :value, :timestamp])
+    |> cast(attrs, [:content, :difficulty, :category, :tag, :additionalData, :userNonce, :txid, :vout, :value])
+    |> validate_required([:content, :difficulty, :category, :tag, :additionalData, :userNonce, :txid, :vout, :value])
   end
 end
--- lib/proofofwork_web/controllers/api/jobs_controller.ex
+++ lib/proofofwork_web/controllers/api/jobs_controller.ex
@@ -9,11 +9,36 @@ defmodule ProofofworkWeb.Api.JobsController do
 
     limit = params["limit"] || 25
     offset = params["offset"] || 0
+    order_by = case !!params["order_by"] do
+      true ->
+        String.to_existing_atom(params["order_by"])
+      false -> 
+        :inserted_at
+    end
+
+    direction = case params["order_direction"] do
+      "asc" -> :asc
+      "desc" -> :desc
+      _ -> :desc
+    end
+
+    where = if params["category"] do
+      [category: params["category"]]
+    else 
+      []
+    end
+
+    where = if params["content"] do
+      [content: params["content"]]
+    else
+      where
+    end
 
     query = from BoostJob,
       limit: ^limit,
       offset: ^offset,
-      order_by: [desc: :inserted_at]
+      order_by: [{^direction, ^order_by}],
+      where: ^where
 
     jobs = Repo.all(query)
     json(conn, %{jobs: jobs})
