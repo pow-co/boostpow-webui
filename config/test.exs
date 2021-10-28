607545ce38c0f53be60904a53ea182c99ceffb58
Steven Zeiler <me@stevezeiler.com>
2021-10-28 10:41:19 +0200

    Test with Postgres on CircleCI

--- config/test.exs
+++ config/test.exs
@@ -6,7 +6,7 @@ use Mix.Config
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :proofofwork, Proofofwork.Repo,
-  username: "zyler",
+  username: "powco_test",
   password: "",
   database: "powco_test#{System.get_env("MIX_TEST_PARTITION")}",
   hostname: "localhost",
